b908c9bdfe323656f357926730ea2143dd3acc32
Baptiste Lesquoy <lesquoy.baptiste@gmail.com>
2017-01-11 21:42:32 +0100

    init

---
+++ src/mason_zombies/Zombie.java
@@ -0,0 +1,14 @@
+package mason_zombies;
+
+import sim.engine.SimState;
+import sim.engine.Steppable;
+
+public class Zombie implements Steppable{
+
+	@Override
+	public void step(SimState state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
