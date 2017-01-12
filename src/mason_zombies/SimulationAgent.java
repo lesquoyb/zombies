f84519dfd61e7e03d88e71a4fae945abf36cdb5e
hormain <hormainloic@hotmail.fr>
2017-01-12 02:56:20 +0100

    lechage de mur on

--- src/mason_zombies/SimulationAgent.java
+++ src/mason_zombies/SimulationAgent.java
@@ -67,18 +67,35 @@ public abstract class SimulationAgent implements Steppable{
 
 
 		movement.multiplyIn(5./max_dist);
-		//if(world.obstacles.field[(int)(me.x-1)][(int)me.y]==1)movement.x+1.;
-
+	
 		movement.setLength(Math.min(movement.length(), max_dist));
 
 		movement.addIn(world.yard.getObjectLocation(this));
 		movement.setX(Math.min(Math.max(0, movement.x), world.yard.width));//on ne sort pas de la map
 		movement.setY(Math.min(Math.max(0, movement.y), world.yard.height));
+		movement=miniBrasenham(me,movement,world);
 		
 		world.yard.setObjectLocation(this, new Double2D(movement));
 	}
-
-
+	
+	public MutableDouble2D miniBrasenham( Double2D p2,MutableDouble2D p1,World world){
+		MutableDouble2D res=new MutableDouble2D();
+		res.x=p2.x;
+		res.y=p2.y;
+		Double2D delta=new Double2D(p1.x-p2.x,p1.y-p2.y);
+		double e=2*delta.y-delta.x;
+		for(int i=1;i<delta.x;i++){
+			while(e>=0){
+				if(world.obstacles.field[(int)res.x][(int)res.y+1]==0)
+					res.y+=1;
+				e-=2*delta.x;
+			}
+			if(world.obstacles.field[(int)res.x+1][(int)res.y]==0)
+				res.x+=1;
+			e+=2*delta.y;
+		}
+		return res;
+	}
 
 
 }
