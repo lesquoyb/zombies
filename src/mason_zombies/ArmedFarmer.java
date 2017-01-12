6f1ef11626d57ff2c746067c0907741d83575798
Baptiste Lesquoy <lesquoy.baptiste@gmail.com>
2017-01-12 01:50:37 +0100

    éviter

--- src/mason_zombies/ArmedFarmer.java
+++ src/mason_zombies/ArmedFarmer.java
@@ -2,6 +2,9 @@ package mason_zombies;
 
 public class ArmedFarmer extends Farmer{
 
+	public ArmedFarmer() {
+		depth_of_view = 3*max_dist;
+	}
 	
 	@Override
 	protected void positionProcessing(World world) {
--- src/mason_zombies/Zombie.java
+++ src/mason_zombies/Zombie.java
@@ -9,13 +9,45 @@ public class Zombie extends SimulationAgent{
 
 	public Zombie() {
 		depth_of_view = 100;
+		max_dist = 0.05;
 	}
+
+
+
 	@Override
 	protected void positionProcessing(World world) {
 
-		movement = friendsBarycenter(world.predators.getEdgesOut(this), world.yard);
-	//	movement.addIn(friendsBarycenter(world.predators.getEdgesIn(this), world.yard).negate());
+		Farmer nearest = null;
+		double dist = Double.MAX_VALUE;
+		Double2D me = world.yard.getObjectLocation(this);
+		Double2D next;
+		for(Farmer f : world.farmers){
+			next = world.yard.getObjectLocation(f);
+			if(next != null){
+				if(me.distance(next) < dist){
+					dist = me.distance(next);
+					nearest = f;
+				}
+			}
+		}
+		for(Farmer f : world.armedFarmers){
+			next = world.yard.getObjectLocation(f);
+
+			if(next != null){
+				if(me.distance(next) < dist){
+					dist = me.distance(next);
+					nearest = f;
+				}
+			}
+		}
+		if( dist< max_dist*max_dist*2 ){
+			world.isEaten(nearest);
+		}
 		
+		Double2D aim = world.yard.getObjectLocation(nearest);
+		movement = new MutableDouble2D(me.x - aim.x , me.y - aim.y);
+		//	movement.addIn(friendsBarycenter(world.predators.getEdgesIn(this), world.yard).negate());
+
 	}
 
 }
